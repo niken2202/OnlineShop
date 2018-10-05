a7da0a32b482f82fb2e7dbac9040adcd41ad5d7f
LeTrongThang <niken2202@gmail.com>
2018-10-05 23:20:34 +0700

    Complete Infrastucture

---
+++ BookShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,20 @@
+﻿using BookShop.Data.Infrastructure;
+using BookShop.Model.Models;
+namespace BookShop.Data
+{
+    public class DbFactory : Disposeable, IDbFactory
+    {
+       private OnlineShopDbContext dbContext;
+        public OnlineShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new OnlineShopDbContext());
+        }
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+            {
+                dbContext.Dispose();
+            }
+        }
+    }
+}
---
+++ BookShop.Data/Infrastructure/Disposeable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop.Data.Infrastructure
+{
+    public class Disposeable : IDisposable
+    {
+        private bool isDisposed;
+        public void Dispose(bool disposing)
+        {
+            if(!isDisposed && disposing)
+            {
+                DisposeCore();
+            }
+            isDisposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~Disposeable()
+        {
+            Dispose(false);
+        }
+        //override this to dispose custom object
+        protected virtual void DisposeCore()
+        {
+
+        }
+    }
+}
